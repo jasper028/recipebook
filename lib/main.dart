95ebbfecb125069d332c044f8876c703a73c0473
jasper028 <pherdavidpineda@gmail.com>
2025-03-21 20:23:29 +0800

    TRy

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
-      title: 'Recipe Book',
+      title: 'Recipe Booksg',
       theme: CupertinoThemeData(
         primaryColor: CupertinoColors.systemBlue,
       ),
