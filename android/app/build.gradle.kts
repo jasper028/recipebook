d1216eaedcfa7f4a5745ca665589cb801c8e397a
jasper028 <pherdavidpineda@gmail.com>
2025-03-21 22:39:30 +0800

    trrte

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,21 +20,28 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.recipebookapp"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        release {
+            storeFile = file("keystore.jks") // Path to the keystore file
+            storePassword = System.getenv("KEYSTORE_PASSWORD") ?: "your-store-password"
+            keyAlias = System.getenv("KEY_ALIAS") ?: "your-key-alias"
+            keyPassword = System.getenv("KEY_PASSWORD") ?: "your-key-password"
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
+            minifyEnabled = true
+            shrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
 }
